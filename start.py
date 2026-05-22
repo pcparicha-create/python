8273ecb6a90eac0c5a83f6a8255b2debf1d1853c
pcparicha-create <pcparicha@gmail.com>
2026-05-22 17:45:44 +1000

    Add files via upload

---
+++ start.py
@@ -0,0 +1,31 @@
+import keyword
+#Print a Sentence
+print("Welcome to the world of programming!")
+
+#Print a Number
+print(4)
+
+#Next Line after print
+print("Hello Python \n")
+
+#Pass multiple arguments for printing
+print("hello", 5)
+
+#End argument of Print Statement
+print("welcome to ", end = "*")
+
+# Creating different variables
+x = 5
+y = "Prad"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+# User Input
+name = input("enter your name: ")
+
+print("\nHello", name, "\nwelcome to codingal")
+
+print("The keyword in Python are: ", keyword.kwlist)
