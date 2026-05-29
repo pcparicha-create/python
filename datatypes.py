a72e623c6f09cbc9f3c1af9cf91ded69bd64b3fe
pcparicha-create <pcparicha@gmail.com>
2026-05-29 17:42:50 +1000

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,18 @@
+# Let's check the datatype of different values
+
+a = 6
+print("type of a: ", type(a))
+
+b = 3.14
+print("type of b: ", type(b))
+
+c = "codi"
+print("type of c: ", type(c))
+
+d = "ngal"
+print("type of d: ", type(d))
+e = True
+print("type of e: ", type(e))
+
+print(c+d)
+
