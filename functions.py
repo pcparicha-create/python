6befc6aacf4ac530f9e64f665e6393d4e4177bd8
pcparicha-create <pcparicha@gmail.com>
2026-07-10 17:36:05 +1000

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,13 @@
+def wishes():
+    print("Hello!")
+    print("How are you?")
+
+wishes()
+
+def weather():
+    print("the weather is pleasent in", autumn )
+    print("the weather is pleasent in", spring )
+
+autumn = "spring"
+spring= autumn
+weather()
