79b9fc049d9de0e219217aa80269b582a678b7cd
pcparicha-create <pcparicha@gmail.com>
2026-07-23 19:26:58 +1000

    Add files via upload

---
+++ exceptionh.py
@@ -0,0 +1,42 @@
+try:
+    number = int(input("enter a number"))
+    print("the number entered is", number)
+
+except ValueError as ex:
+    print("Exception:", ex)
+
+
+try:
+    num1, num2 = eval(input("Enter two numbers, separated by a comma: "))
+    result = num1 / num2
+    print("Result is:", result)
+
+# using multiple except block for different type of error
+
+except ZeroDivisionError:
+    print("Division by zero is error !!")
+
+except SyntaxError:
+    print("Comma is missing. Enter numbers separated by comma like this 1, 2")
+
+except:
+    print("Wrong input")
+
+else:
+    print("No exceptions")
+
+finally:
+    print("This will execute no matter what")
+
+
+valid = False
+while not valid:
+    try:
+        n= int(input("Number:"))
+        while n%2==0:
+            print("Bye!")
+        valid = True
+
+    except ValueError:
+        print ("invalid")
+
